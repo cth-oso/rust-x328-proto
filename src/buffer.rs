36af6d21ea6f8ba8bc7cf3dc29057799b124c478
Lukas Sandström <lukas.sandstrom@chalmers.se>
2021-05-05 22:28:52 +0200

    Don't panic() if writing more than the total buffer capacity
    arrayvec.drain(range) panics if the given range is outside
    the bounds of the array. Fix this by only storing the tail
    of the data to be written.

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,8 +1,10 @@
 use arrayvec::ArrayVec;
 
+const BUF_SIZE: usize = 40; // The maximum X3.28 message length is 18 bytes
+
 #[derive(Debug)]
 pub struct Buffer {
-    data: ArrayVec<u8, 40>, // The maximum X3.28 message length is 18 bytes
+    data: ArrayVec<u8, BUF_SIZE>,
     read_pos: usize,
 }
 
@@ -23,13 +25,20 @@ impl Buffer {
         self.read_pos += len;
     }
 
-    pub fn write(&mut self, bytes: &[u8]) {
+    pub fn write(&mut self, mut bytes: &[u8]) {
         if self.read_pos == self.data.len() {
             self.clear();
         }
-        let cap = self.data.remaining_capacity();
-        if cap < bytes.len() {
-            self.data.drain(..(bytes.len() - cap));
+        if bytes.len() > self.data.capacity() {
+            bytes = &bytes[(bytes.len() - self.data.capacity())..];
+            self.clear();
+        } else {
+            let cap = self.data.remaining_capacity();
+            if cap < bytes.len() {
+                let drain_len = bytes.len() - cap;
+                self.data.drain(..drain_len);
+                self.read_pos = self.read_pos.saturating_sub(drain_len);
+            }
         }
         let write_pos = self.data.len();
         self.data.try_extend_from_slice(bytes).unwrap();
@@ -66,5 +75,30 @@ mod tests {
     fn test_slice() {
         let buf = get_buffer();
         assert_eq!(buf.as_ref().len(), buf.len());
+    }
+
+    #[test]
+    fn buffer_spill() {
+        let mut buf = Buffer::new();
+        for _ in 0..(BUF_SIZE + 1) {
+            buf.write(b"a");
+        }
+        assert_eq!(buf.read_pos, 0);
+        buf.consume(5);
+        assert_eq!(buf.read_pos, 5);
+        buf.write(b"1234");
+        assert_eq!(buf.read_pos, 1);
+        buf.write(b"5");
+        assert_eq!(buf.read_pos, 0);
+        buf.write(b"67");
+        assert_eq!(buf.read_pos, 0);
+    }
+
+    #[test]
+    fn too_large_write() {
+        let mut buf = Buffer::new();
+        let data: String = std::iter::once("abc").cycle().take(BUF_SIZE).collect();
+        buf.write(data.as_bytes());
+        assert_eq!(buf.data, data.as_bytes()[(data.len() - BUF_SIZE)..])
     }
 }
